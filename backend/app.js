6d1e68f2d0a46d8db00a4d91e629a8a46e29eb3b
Nikhil Mohanty <nikhilmohanty443@gmail.com>
2025-12-27 15:13:08 +0530

    fixed cors issue

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: ["http://localhost:3000", "https://techthrive.vercel.app"], credentials: true }));
 
 // Route Imports
 const product = require("./routes/productRoute");
